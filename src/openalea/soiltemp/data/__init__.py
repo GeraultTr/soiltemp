a15a7ed26377ffb1122a1d289caa40e7029974cd
pradal <christophe.pradal@cirad.fr>
2025-06-17 11:56:22 +0200

    Fix data import

--- src/openalea/soiltemp/data/__init__.py
+++ src/openalea/soiltemp/data/__init__.py
@@ -1,3 +1,7 @@
-import importlib.resources as resources
+try:
+    import importlib_resources as resources
+except ImportError:
+    from importlib import resources
+    
 
 data_dir = resources.files(__name__)
